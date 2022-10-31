800eb27205e859f48b6a6499be5056d0c5500e65
Spyridon Karagiannis <s.karagiannis@iit.demokritos.gr>
2022-10-31 10:52:49 +0200

    sale invoices and goods models and migrations

---
+++ app/Models/SaleInvoices.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SaleInvoices extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'hashID', 'sale_invoiceID', 'seira', 'date', 'paid', 'payment_method', 'mark'
+    ];
+
+    public function services()
+    {
+        return $this->hasMany(Goods::class, 'sale_invoice_id', 'hashID');
+    }
+}
---
+++ database/migrations/2022_10_31_083725_create_sale_invoices_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSaleInvoicesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sale_invoices', function (Blueprint $table) {
+            $table->id();
+            $table->string('hashID')->unique();
+            $table->string('seira')->default('ΑΝΕΥ');
+            $table->unsignedBigInteger('sale_invoiceID');
+            $table->date('date');
+            $table->boolean('paid');
+            $table->unsignedBigInteger('payment_method')->nullable()->default(5);
+            $table->string('mark')->unique()->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sale_invoices');
+    }
+}
